3aa3bca7a0217197e09a3626f6dd722b936e921d
Estefanía Santana <83146564+tefsantana@users.noreply.github.com>
2021-11-17 04:48:29 -0300

    Add files via upload

---
+++ TPDos/build/generated-sources/ap-source-output/logica/Cliente_.java
@@ -0,0 +1,22 @@
+package logica;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2021-11-17T00:55:39", comments="EclipseLink-2.7.7.v20200504-rNA")
+@StaticMetamodel(Cliente.class)
+public class Cliente_ { 
+
+    public static volatile SingularAttribute<Cliente, String> num_cliente;
+    public static volatile SingularAttribute<Cliente, String> nombre_perro;
+    public static volatile SingularAttribute<Cliente, String> raza;
+    public static volatile SingularAttribute<Cliente, String> nombre_duenio;
+    public static volatile SingularAttribute<Cliente, String> color;
+    public static volatile SingularAttribute<Cliente, String> alergico;
+    public static volatile SingularAttribute<Cliente, String> observaciones;
+    public static volatile SingularAttribute<Cliente, String> atencion_especial;
+    public static volatile SingularAttribute<Cliente, Integer> id;
+    public static volatile SingularAttribute<Cliente, String> celular_duenio;
+
+}
